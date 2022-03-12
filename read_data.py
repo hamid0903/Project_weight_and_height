fada2481676d480b57c361e5e65f4b805859bad9
Hamid Kamalov <hamid_kamalov@mail.ru>
2022-03-12 08:33:03 +0500

    READ

--- read_data.py
+++ read_data.py
@@ -10,5 +10,9 @@ def read_csv_data(file_path):
     """
     result_data = []
     # WRITE YOUR CODE HERE
+    data=open('data\weight-height.csv').read()
+    result_data=data.split('\n')
     return result_data
+file_path='data\weight-height.csv'
+read_csv_data(file_path)
     
